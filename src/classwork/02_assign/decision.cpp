831ee8a267af55333349c9c0dca074aab23e3ce2
naziaislam91 <nazia.islam@g.austincc.edu>
2020-02-06 19:38:25 -0600

    Classwork 02_assign (gross_pay)

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -1,1 +1,24 @@
+#include "decision.h"
 //Write the function code that returns the product of hours and hourly_rate.
+
+double gross_pay(double hours, double hourly_rate)
+{
+	double results;
+	if (hours <= 40)
+	{
+		double regular_pay = hours * hourly_rate;
+		 results = regular_pay;
+	}
+	else if (hours > 40)
+	{
+		double regular_pay = hours * hourly_rate;
+		double overtime_pay = 1.5 * (hours - 40) * hourly_rate;
+		results = regular_pay + overtime_pay;
+		
+	}
+
+	
+
+	
+	return results;
+}
--- src/examples/02_module/03_switch/main.cpp
+++ src/examples/02_module/03_switch/main.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
+#include "switch.h"
+
+using std::cin;
+using std::cout;
 
 int main() 
 {
-	
+	int Option;
+	cout << "Enter a number: ";
+	cin >> Option;
+
+	string result = menu(Option);
+	cout << result;
 
 	return 0;
 }
