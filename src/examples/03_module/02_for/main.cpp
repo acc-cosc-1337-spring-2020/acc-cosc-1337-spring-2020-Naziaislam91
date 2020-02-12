5f8420c389a90ee8a4a133e7dc1009dd95b715dd
naziaislam91 <nazia.islam@g.austincc.edu>
2020-02-11 18:39:27 -0600

    for loop

--- src/examples/03_module/02_for/main.cpp
+++ src/examples/03_module/02_for/main.cpp
@@ -1,7 +1,14 @@
 #include"for.h"
+#include<iostream>
+using std::cout; using std:: cin;
 
 int main() 
 {
+	int num;
+	cout << "enter a number: ";
+	cin >> num;
+
+	display_numbers(num);
 	
 	return 0;
 }
--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -1,7 +1,23 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "decisions.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test get grade point function") {
+	REQUIRE(get_grade_points("A") == 4);
+	REQUIRE(get_grade_points("B") == 3);
+	REQUIRE(get_grade_points("C") == 2);
+	REQUIRE(get_grade_points("D") == 1);
+	REQUIRE(get_grade_points("F") == 0);
+
+}
+
+TEST_CASE("Calculate GPA")
+{
+	REQUIRE(calculate_gpa(12, 45.0) == 3.75);
+	REQUIRE(calculate_gpa(120, 390.0) == 3.25);
+	REQUIRE(calculate_gpa(90, 180.0) == 2.00);
+}
