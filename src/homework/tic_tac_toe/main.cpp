732c0b03abd136a7e5919d19434a56ad2e95b735
nazia islam <naziaislam91@gmail.com>
2020-03-03 23:31:41 -0600

    Homework Tic tak toe

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,10 @@
+#include "tic_tac_toe.h"
+#include <iostream>
 
+using std::cout;
 int main() 
 {
+	cout << "The first player: ";
 
 	return 0;
 }
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,14 @@
 //h
+#include<string>
+
+
+class Error
+{
+public:
+	Error(std::string msg) : message {msg} {}
+	std::string get_message() const { return message; }
+
+private:
+	std::string message;
+	//class member variable 
+};
