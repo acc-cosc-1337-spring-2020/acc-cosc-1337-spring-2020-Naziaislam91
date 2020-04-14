7327277d79726e2862ccdd8b51febcfe521c95b3
nazia islam <naziaislam91@gmail.com>
2020-04-14 00:05:06 -0500

    homework 9

--- src/homework/tic_tac_toe/tic_tac_toe_4.h
+++ src/homework/tic_tac_toe/tic_tac_toe_4.h
@@ -1,1 +1,19 @@
-//h
+#include "tic_tac_toe.h"//h
+#ifndef TicTacToe_four_H 
+#define TicTacToe_four_H 
+class TicTacToeFour : public TicTacToe
+{
+public:
+
+
+
+private:
+	bool check_column_win();
+	bool check_row_win();
+	bool check_diagonal_win();
+
+	std::vector<std::string> pegs{ 16, " " };
+
+};
+
+#endif
